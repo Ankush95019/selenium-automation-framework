907ea638e471d9185013f3508f5197ec311caa8b
Ankush <ankush4031@gmail.com>
2026-07-24 00:11:47 +0530

    Added Test Case

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -36,6 +38,18 @@ public class BaseTest {
 
     }
 
+    @Test
+    public void verifyGoogleTitle() {
+
+        driver.get("https://www.google.com");
+
+        String title = driver.getTitle();
+
+        System.out.println(title);
+
+        Assert.assertEquals(title, "Google");
+
+    }
 
 
     @AfterMethod
