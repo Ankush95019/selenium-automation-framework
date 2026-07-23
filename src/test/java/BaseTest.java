e25028776d331fadc64ae74ffd1ea9a6517bacb9
Ankush <ankush4031@gmail.com>
2026-07-24 00:05:19 +0530

    Configure Chrome headless mode for CI

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,43 +1,51 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
 public class BaseTest {
-	
-	WebDriver driver;
+
+
+    public WebDriver driver;
+
 
     @BeforeMethod
-    public void setup() {
+    public void setup(){
+
 
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+
+        driver = new ChromeDriver(options);
+
 
         driver.manage().window().maximize();
-    }
-
-    @Test
-    public void verifyGoogleTitle() {
-
-        driver.get("https://www.google.com");
-
-        String title = driver.getTitle();
-
-        System.out.println(title);
-
-        Assert.assertEquals(title, "Google");
 
     }
+
+
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(){
 
-        driver.quit();
+        if(driver != null){
+
+            driver.quit();
+
+        }
 
     }
 
